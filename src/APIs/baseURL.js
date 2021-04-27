b0148ffa34dc5326f38c33ea35dcccfb03c2635b
LeANguyen <scorcher159@gmail.com>
2021-04-27 11:55:00 +0700

    config img src

--- src/APIs/baseURL.js
+++ src/APIs/baseURL.js
@@ -1,5 +1,5 @@
 // https://generic-fashion-backend.herokuapp.com
 // http://localhost:3000
-const baseURL = "http://localhost:3000";
-// const baseURL = "https://generic-fashion-backend.herokuapp.com";
+// const baseURL = "http://localhost:3000";
+const baseURL = "https://generic-fashion-backend.herokuapp.com";
 export default baseURL;
